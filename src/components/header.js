726482983da508f1374481bc395633ca69e740f8
cascutter <cascutter13@gmail.com>
2020-12-04 17:51:36 -0600

    Imported css

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,12 @@
 import React from "react";
 
+import "../styles/header.css"
+
 function Header() {
     return (
-        <div className="bg-white shadow text-gray-800">
-            <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <h1 className="font-bold">Employee Directory</h1>
-                </div>
-                <a href="https://github.com/cascutter/React-Employee-Directory" className="hover:text-yellow-500">GitHub Repo</a>
-            </header>
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Search by First Name or sort by Name or Department</p>
         </div>
     )
 }
