7071affafd12c36eab067cc478235fa22027c863
cascutter <cascutter13@gmail.com>
2020-12-04 17:50:48 -0600

    Adjusted grid layout and div tags

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,19 +1,21 @@
 import React from "react";
 
+import "../styles/employeecard.css";
+
 function EmployeeCard({ name, image, dept, email, phone }) {
     return (
-        <div className="md:flex bg-white shadow text-gray-800 my-4 py-4 px-10 rounded-md items-center justify-between hover:bg-gray-300">
-        <img
-            style={{ maxWidth: "60px"}}
-            className="rounded-full shadow-md border border-gray-300"
-            src={image}
-            alt="employee"
-        />
-        <p className="font-bold text-md">{name}</p>
-        <p>{dept}</p>
-        <p className="text-blue-500">{email}</p>
-        <p>{phone}</p>
-    </div>
+        <div className="card">
+            <div><img
+                style={{ maxWidth: "110px"}}
+                className="image"
+                src={image}
+                alt="employee"
+            /></div>
+            <div>{name}</div>
+            <div>{dept}</div>
+            <div>{email}</div>
+            <div>{phone}</div>
+        </div>
     )
 };
 
