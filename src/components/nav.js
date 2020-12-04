99e3ead0fc9576a55fa47a8ced9bba9c1d7c7868
cascutter <cascutter13@gmail.com>
2020-12-04 17:51:55 -0600

    Imported css

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,24 @@
 import React from "react";
 
+import "../styles/nav.css";
+
 function Navigation({ onSearch, searchTerm, handleSortByName, handleSortByDept }) {
     return (
-        <div className="md:flex justify-between my-6">
-            <div className="md:mb-0 mb-4">
-                <button onClick={handleSortByName} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 mr-4 hover:bg-yellow-500 hover:text-white">Name</button>
-                <button onClick={handleSortByDept} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 hover:bg-yellow-500 hover:text-white">Dept</button>
-            </div>
-            <form>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <form className="form-inline">
                 <input
+                    className="form-control"
                     value={searchTerm} 
                     onChange={onSearch} 
-                    className="shadow pl-5 rounded w-full py-1"
                     type="text"
-                    placeholder="search employee"/>
+                    placeholder="search employees"
+                />
             </form>
-        </div>
+            <p>Sort Employees By:</p>
+            <button type="button" class="btn" onClick={handleSortByName}>Name</button>
+            <button type="button" class="btn" onClick={handleSortByDept}>Department</button>
+            
+        </nav>
     )
 }
 
